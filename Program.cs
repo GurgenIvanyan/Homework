66adb2f517425289ebeba2ab52dc3bc6d57af420
GurgenIvanyan <gurgenivanyan9812@icloud.com>
2025-03-21 17:10:41 +0400

    Translator and Shape

--- Program.cs
+++ Program.cs
@@ -1,143 +1,87 @@
-﻿namespace _arajadranq2;
+﻿namespace arajadranq4;
 
-public class Course
+public abstract class Shape
 {
-    public string Name { get; set; }
-    public int Monthfee{get; set;}
-    public Modul[] Moduls { get; set; }
+    public abstract double Surface();
+    public abstract void Draw();
 
-    public Course(string name, int monthfee, Modul[] moduls)
+    public void print()
     {
-        Name = name;
-        Monthfee = monthfee;
-        Moduls = moduls;
+     Console.WriteLine($"{GetType().Name}   S={Surface()}");
+     Console.WriteLine();
+     Draw();
     }
 }
 
-public class Modul
+public class Square : Shape
 {
-    public string Title { get; set; }
-    public double Duration{get; set;}
+    private double side { get; set; }
 
-    public Modul(string title, double duration)
+    public Square(double side)
     {
-        Title = title;
-        Duration = duration;
+        this.side = side;
+    }
+
+    public override double Surface()
+    {
+        return side * side;
+    }
+
+    public override void Draw()
+    {
+        for (int i = 0; i < side; i++)
+        {
+            for (int j = 0; j < side; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
 
-public class Web : Course
-{
-    public string Type{get; set;}
-
-    public Web(string name, int monthfee, string Type,Modul[] moduls) : base(name, monthfee, moduls)
+public class Rectangle : Shape
     {
-        this.Type = Type;
-    }
-}
+        private double Width{ get; set; }
+        private double Height{ get; set; }
 
-public class Game : Course
-{
-    public string Engine{get; set;}
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
 
-    public Game(string name, int monthfee,string Engine,Modul[] moduls) : base(name, monthfee, moduls)
-    {
-        this.Engine = Engine;
-    }
-}
+        public override double Surface()
+        {
+            return Height * Width;
+        }
 
-public class Group
-{
-    public int StudentCount { get; set; }
-    public string Name;
-    public Course Course { get; set; }
+        public override void Draw()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
 
-    public Group(string name, int studentCount, Course course)
-    {
-        Name = name;
-        Course = course;
-        StudentCount = studentCount;
-    }
-    
-}
-public class Ai:Course
-{
-    public Ai(string name, int monthfee, Modul[] moduls) : base(name, monthfee, moduls)
-    {
-        
-    }
-    
-}
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Course[] courses = new Course[]
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new Square(5);
+        shapes[1] = new Rectangle(3, 4);
+        foreach (Shape shape in shapes)
         {
-            new Web("Frontend Development", 500000, "frontend", new Modul[]
-            {
-                new Modul("HTML & CSS", 2), new Modul("JavaScript", 3)
-            }),
-            new Web("Fullstack Development", 60000, "fullstack", new Modul[]
-            {
-                new Modul("Node.js", 4), new Modul("React", 5)
-            }),
-            new Ai("Machine Learning", 80000, new Modul[]
-            {
-                new Modul("Python for AI", 6), new Modul("Deep Learning", 8)
-            }),
-            new Game("Game Development", 72000, "Unity", new Modul[]
-            {
-                new Modul("C# for Unity", 5), new Modul("Physics in Games", 4)
-            }),
-            new Game("Game Dev Advanced", 42000, "Unreal", new Modul[]
-            {
-                new Modul("Blueprints", 6), new Modul("C++ for Unreal", 7)
-            })
-        };
-
-        Group[] groups = new Group[]
-        {
-            new Group("Frontend Group 1", 15, courses[0]),
-            new Group("Fullstack Group 1", 12, courses[1]),
-            new Group("AI Group 1", 10, courses[2]),
-            new Group("Game Dev Group 1", 20, courses[3]),
-            new Group("Game Dev Group 2", 18, courses[4])
-        };
-        int WebMembersCount = 0;
-        for (int i = 0; i < groups.Length; i++)
-        {
-            if (groups[i].Course is Web)
-            {
-                WebMembersCount += groups[i].StudentCount;
-            }
-        }
-
-        Console.WriteLine("Web members count: " + WebMembersCount);
-        double IncomeFromGame = 0;
-        for (int i = 0; i < groups.Length; i++)
-        {
-            if (groups[i].Course is Game game && game.Engine == "Unreal")
-            {
-                IncomeFromGame += groups[i].StudentCount * groups[i].Course.Monthfee;
-            }
-        }
-        Console.WriteLine($"Income from game: {IncomeFromGame}");
-        int maxMember = 0;
-        foreach (var group in groups)
-        {
-            if (group.StudentCount > maxMember)
-            {
-                maxMember = group.StudentCount;
-            }
-        }
-
-        for (int i = 0; i < groups.Length; i++)
-        {
-            if (groups[i].StudentCount == maxMember)
-            {
-                Console.WriteLine($"the most popolar Cours is  {groups[i].Course.Name} with {groups[i].StudentCount} students");
-            }
+            shape.print();
         }
     }
 }
