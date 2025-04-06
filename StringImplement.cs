e9666354f623f57b694f07455ab5aa74bc04caa3
GurgenIvanyan <gurgenivanyan9812@icloud.com>
2025-04-07 01:12:04 +0400

    String implementation and Operator overloading

---
+++ StringImplement.cs
@@ -0,0 +1,139 @@
+﻿using System.Text;
+using System.Collections.Generic;
+namespace StringImplement;
+
+static class Extensions
+{
+    public static List<string> MySplit(this string str, char separator)
+    {
+        int start = 0;
+        List<string> parts = new List<string>();
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == separator)
+            {
+                parts.Add(str.Substring(start, i - start));
+                start = i + 1;
+            }
+        }
+        parts.Add(str.Substring(start));
+        return parts;
+    }
+
+    public static int IndexOf(this string str, string search)
+    {
+        if (str == null || search == null || str.Length == 0 || search.Length > str.Length)
+        {
+            return -1;
+        }
+        
+        for (int i = 0; i <= str.Length - search.Length; i++)
+        {
+            bool found = true;
+            for (int j = 0; j < search.Length; j++)
+            {
+                if (str[i + j] != search[j])
+                {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static bool Contains(this string str, string search)
+    {
+        return str.IndexOf(search) != -1;
+    }
+
+    public static string Substring(this string str, int start, int length)
+    {
+        string result = string.Empty;
+        for (int i = start; i <= length; i++)
+        {
+            result += str[i];
+        }
+
+        return result;
+    }
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        Random random = new Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            int index = random.Next(n--);
+            T temp = list[index];
+            list[index] = list[n];
+            list[n] = temp;
+        }
+    }
+
+    public static void Reverse<T>(this IList<T> list)
+    {
+        for (int i = list.Count/2; i >= 0; i--)
+        {
+            T temp = list[i];
+            list[i] = list[list.Count - 1 - i];
+            list[list.Count - 1 - i] = temp;
+        }
+    }
+
+    public static List<T> Slice<T>(this IList<T> list, int start, int length)
+    {
+        List<T> slice = new List<T>();
+        for (int i = start; i < length; i++)
+        {
+            slice.Add(list[i]);
+        }
+        return slice;
+    }
+
+    public static T TAt<T>(this IList<T> list, int index)
+    {
+        if (index >= list.Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        if (index < 0)
+        {
+            return list[list.Count - index];
+        }
+        return list[index];
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sb = "Anna, has, a Car"; 
+        List<string> result =Extensions.MySplit(sb, ',');
+        foreach (var part in result) 
+        {
+            
+            Console.WriteLine(part.Trim()); 
+        }
+
+        Console.WriteLine(Extensions.IndexOf(sb, "nn"));
+        Console.WriteLine(Extensions.Substring(sb,1, 4));
+        List<string> workers = new List<string>() { "Anna", "Bob", "Jack" };
+        Extensions.Shuffle(workers);
+        foreach (string name in workers)
+        {
+            Console.WriteLine(name);
+        }
+        Extensions.Reverse(workers);
+        foreach (string name in workers)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ StudentOperatorOverload.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StudentOverload
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Student student && student.Id == Id)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public Student(string name, int id)
+        {
+            this.Name = name;
+            this.Id = id;  
+        }
+    }
+
+    class Group
+    {
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public static bool operator true(Group group)
+        {
+            return group.Students.Count > 0;
+        }
+
+        public static bool operator false(Group group)
+        {
+            return group.Students.Count == 0;
+        }
+
+       
+        public static Group operator +(Group group1, Group group2)
+        { 
+            var newGroup = new Group();
+            newGroup.Students = group1.Students.Concat(group2.Students).Distinct().ToList();
+            return newGroup;
+        }
+
+        public bool Contains(Student student)
+        {
+            return Students.Contains(student);
+        }
+
+        public static Group operator -(Group group1, Group group2)
+        {
+            var newGroup = new Group();
+            foreach (var student in group1.Students) 
+            {
+                if (!group2.Students.Contains(student))
+                {
+                    newGroup.Students.Add(student);
+                }
+            }
+            return newGroup;
+        }
+
+        public void PrintStudents()
+        {
+            foreach (var student in Students)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group1 = new Group();
+            Group group2 = new Group();
+            group1.Students.AddRange(new[]
+            {
+                new Student("John", 1),
+                new Student("Jane", 2),
+                new Student("Jack", 3),
+            });
+            group2.Students.AddRange(new[]
+            {
+                new Student("John", 1),
+                new Student("Ani", 5),
+                new Student("Anna", 6),
+            });
+            
+            var group3 = group1 + group2; 
+            Console.WriteLine("Members of group3");
+            group3.PrintStudents();  
+            var group4 = group1 - group2;
+            Console.WriteLine("Members of group4");
+            group4.PrintStudents();
+        }
+    }
+}
