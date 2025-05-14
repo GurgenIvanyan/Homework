36cf300240bc802f0421fefd4d987d9750ae7c99
GurgenIvanyan <gurgenivanyan9812@icloud.com>
2025-05-14 13:51:49 +0400

    Threads... Monitor and SemaphoreSlim

--- Program.cs
+++ Program.cs
@@ -1,40 +1,9 @@
-﻿namespace ConsoleApp26;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace Toms;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-     
-        var students = new List<(int Id, string Name)>
-        {
-            (1, "Alice"),
-            (2, "Bob"),
-            (3, "Charlie")
-        };
-
-        var submissions = new List<(int StudentId, string AssignmentName)>
-        {
-            (1, "Math HW1"),
-            (1, "Math HW2"),
-            (2, "Science HW1")
-        };
-
-        var result = from student in students
-            join submission in submissions on student.Id equals submission.StudentId into studentSubmissions
-            select new
-            {
-                Name = student.Name,
-                SubmissionCount = studentSubmissions.Count()
-            } into studentWithCount
-            orderby studentWithCount.SubmissionCount descending
-            select studentWithCount;
-        foreach (var student in result)
-        {
-            Console.WriteLine($"{student.Name}: {student.SubmissionCount} submissions");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
